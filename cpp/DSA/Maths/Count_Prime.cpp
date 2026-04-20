528dfce3399913c243d1c5febec0f3f43a89325b
harshad godase <harshadgodase7k@gmail.com>
2026-04-20 15:54:53 +0530

    new files added

---
+++ cpp/DSA/Maths/Count_Prime.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ int countPrimes(int n) {
+        vector<bool>isPrime(n+1,true);
+        int count =0;
+
+        for(int i=2;i<n;i++){
+
+            if(isPrime[i]){
+                count++;
+
+            for(int j=i*2;j<n;j=j+i){
+                isPrime[j]=false;
+            }    
+
+         }
+
+        }
+        return count;        
+    }
+
+    int main(){
+        int n=10;
+        cout<<countPrimes(n);
+        return 0;
+    }
